2b08f8ce38a66c81b5f8b896f0ad9afac77f8ded
Justin Rogers <justin.rogers@gmail.com>
2012-06-19 14:25:54 -0400

    Made the python script actually run the function.

--- 4-SoftwareEngineering/test_animals.py
+++ 4-SoftwareEngineering/test_animals.py
@@ -6,10 +6,15 @@ def test_read_animals():
     ref_time=['21:06','14:12','10:24','20:08','18:46']
     ref_number=[36,25,26,31,20]
     if time != ref_time:
-        print 'Times do not match!1'
+        print 'Times do not match!'
+    else:
+        print 'Times ok!'
     if number != ref_number:
-        print 'Numbers do not match!1'
+        print 'Numbers do not match!'
+    else:
+        print 'Animal numbers ok!'
 
+test_read_animals()
 
 def test_mean(filename):
     d,t,a,number=animals.ranimals(filename)
